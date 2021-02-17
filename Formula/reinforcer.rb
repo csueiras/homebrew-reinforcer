d52cf461f5c7ec803f52037d529f540669d42283
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-17 08:51:22 -0500

    Brew formula update for reinforcer version v0.5.1-alpha

--- Formula/reinforcer.rb
+++ Formula/reinforcer.rb
@@ -5,20 +5,16 @@
 class Reinforcer < Formula
   desc "Codegen tool for easy adoption of resiliency patterns in Go"
   homepage "https://github.com/csueiras/reinforcer"
-  version "0.4.1-alpha"
+  version "0.5.1-alpha"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/csueiras/reinforcer/releases/download/v0.4.1-alpha/reinforcer_0.4.1-alpha_Darwin_x86_64.tar.gz"
-    sha256 "d2407c399034267a9fef304417c8053bbebd995746ff8bee12692ab4e75a21cf"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/csueiras/reinforcer/releases/download/v0.5.1-alpha/reinforcer_0.5.1-alpha_Darwin_x86_64.tar.gz"
+    sha256 "403a501ff45e3244dff2e0b43151ff368376b86d26ba996f96696c38456d3c47"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/csueiras/reinforcer/releases/download/v0.4.1-alpha/reinforcer_0.4.1-alpha_Linux_x86_64.tar.gz"
-    sha256 "b979e10ea7ad65658c873444c197b01b6e4cb45ccc972332c9cf11d2823640f8"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/csueiras/reinforcer/releases/download/v0.4.1-alpha/reinforcer_0.4.1-alpha_Linux_arm64.tar.gz"
-    sha256 "c1acfff29694d6c1f334de32b475dc87d5adfa5d7a7f257abde25d896f33ef1d"
+    url "https://github.com/csueiras/reinforcer/releases/download/v0.5.1-alpha/reinforcer_0.5.1-alpha_Linux_x86_64.tar.gz"
+    sha256 "639c3c67e55135c3a9e5368275bd97b530115f4822561c26839a50a715a758fa"
   end
 
   def install
