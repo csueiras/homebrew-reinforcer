400824a83654a693a85ebeedb0a6514979529177
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-29 19:20:39 -0400

    Brew formula update for reinforcer version v0.9.1

--- Formula/reinforcer.rb
+++ Formula/reinforcer.rb
@@ -5,16 +5,16 @@
 class Reinforcer < Formula
   desc "Codegen tool for easy adoption of resiliency patterns in Go"
   homepage "https://github.com/csueiras/reinforcer"
-  version "0.9.0"
+  version "0.9.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/csueiras/reinforcer/releases/download/v0.9.0/reinforcer_0.9.0_Darwin_x86_64.tar.gz"
-    sha256 "b2f2ce0820dc2395995951b1e3a09c143e1a20d98c5814199fc3cf027874b322"
+    url "https://github.com/csueiras/reinforcer/releases/download/v0.9.1/reinforcer_0.9.1_Darwin_x86_64.tar.gz"
+    sha256 "c6d96e35f83302d1f4af2f7bbc697b7e21af2215e71e75fb32446aca0441e0a1"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/csueiras/reinforcer/releases/download/v0.9.0/reinforcer_0.9.0_Linux_x86_64.tar.gz"
-    sha256 "6e2ccd53c25ae2033685c1c5c747ef15b822e6223fd7b94052c4b73140b09b4e"
+    url "https://github.com/csueiras/reinforcer/releases/download/v0.9.1/reinforcer_0.9.1_Linux_x86_64.tar.gz"
+    sha256 "ffef539d29646b2c42d9cfe141856ec32b9c1c32d67e9fc89772f016644073ae"
   end
 
   def install
